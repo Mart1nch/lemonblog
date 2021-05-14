f290c4c71763c803be304f2eb7f67ecbc7a6ea7c
matian <matian972@qq.com>
2021-05-15 00:29:24 +0800

    初始化2021/05/15

---
+++ src/main/java/com/martin/lemonblog/LemonblogApplication.java
@@ -0,0 +1,13 @@
+package com.martin.lemonblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemonblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/martin/lemonblog/LemonblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.lemonblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemonblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
