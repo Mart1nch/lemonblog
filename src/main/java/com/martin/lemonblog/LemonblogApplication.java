0fccee3ed7341f32fea79629daea57c1cd6d906e
matian <matian972@qq.com>
2021-05-15 15:49:08 +0800

    初始化2121/05/15

--- src/main/java/com/martin/lemonblog/LemonblogApplication.java
+++ src/main/java/com/martin/lemonblog/LemonblogApplication.java
@@ -2,8 +2,9 @@ package com.martin.lemonblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LemonblogApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/martin/lemonblog/controller/BootController.java
@@ -0,0 +1,24 @@
+package com.martin.lemonblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Description TODO
+ * @Date 2021/5/15
+ * @Author by matian
+ */
+@Controller
+public class BootController {
+    @ResponseBody
+    @RequestMapping("/helloworld")
+    public String hello() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/demo")
+    public String demo() {
+        return "demo";
+    }
+}
